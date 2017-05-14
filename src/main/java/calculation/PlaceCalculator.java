5d272b8ced73be2ed0fe6e0d2a6d0db52a256700
Tomas Bartnykas <tomas.bartnykas@gmail.com>
2017-05-14 14:44:33 +0300

    Removed unused code.

--- src/main/java/calculation/PlaceCalculator.java
+++ src/main/java/calculation/PlaceCalculator.java
@@ -2,7 +2,6 @@ package calculation;
 
 import data.Athlete;
 
-import java.math.BigDecimal;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,26 +9,21 @@ import java.util.Map;
 public class PlaceCalculator {
 
     public static Map<String, String> getPlaces(List<Athlete> source) {
-        int excludeLastOne = 1;
         Map<String, String> places = new LinkedHashMap<>();
         long place = 1;
         long startPlace = place;
         for (Athlete athlete : source) {
-            long samePlacesCount = checkCountSamePointsValue(athlete.getTotalPoints(), source);
+            long samePlacesCount = source.stream()
+                    .filter(participant -> athlete.getTotalPoints().compareTo(athlete.getTotalPoints()) == 0).count();
             if (samePlacesCount > 1) {
-                places.put(athlete.getName(), Long.toString(startPlace) + "-" + (startPlace + samePlacesCount - excludeLastOne));
-                place= startPlace + samePlacesCount;
+                int excludeLastOne = 1;
+                places.put(athlete.getName(), startPlace + "-" + (startPlace + samePlacesCount - excludeLastOne));
+                place = startPlace + samePlacesCount;
             } else {
                 places.put(athlete.getName(), Long.toString(place));
-                place++;
-                startPlace = place;
+                startPlace = ++place;
             }
         }
         return places;
     }
-
-    private static long checkCountSamePointsValue(BigDecimal totalPoints, List<Athlete> source) {
-        return source.stream().filter(athlete -> athlete.getTotalPoints().compareTo(totalPoints) == 0).count();
-    }
-
 }
